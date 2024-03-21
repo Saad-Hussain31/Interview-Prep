c4249bca26c78cdbb63557c7c42233a19ea4d15e
Saad-Hussain31 <saadhussain31@gmail.com>
2024-03-21 20:13:14 +0000

    write exception safe copy ctor and copy asg operator

--- exceptional_cpp/exp_safety1.cpp
+++ exceptional_cpp/exp_safety1.cpp
@@ -5,6 +5,7 @@ exception-neutral (propagates all exceptions to the caller,
 without causing integrity problems in a Stack object)
 
 */
+#include <cassert>
 #include <iostream>
 using namespace std;
 
@@ -12,12 +13,30 @@ template <class T> class Stack {
 public:
   Stack();
   ~Stack();
+  Stack(const Stack<T> &other);
+  Stack &operator=(const Stack<T> &other);
 
-private:
+public:
   T *v_;         // ptr to a memory area big
   size_t vsize_; // enough for 'vsize_' T's
   size_t vused_; // # of T's actually in use
 };
+
+template <class T> T *NewCopy(T *src, size_t src_size, size_t dest_size) {
+  assert(dest_size >= src_size);
+
+  // operator=() must guarantee that if it does throw,
+  // then the assigned-to T object must be destructible.
+  T *dest = new T[dest_size];
+
+  try {
+    std::copy(src, src + src_size, dest);
+  } catch (...) {
+    delete[] dest;
+    throw;
+  }
+  return dest;
+}
 
 template <class T>
 Stack<T>::Stack()
@@ -28,4 +47,21 @@ Stack<T>::Stack()
 
 template <class T> Stack<T>::~Stack() {
   delete[] v_; // this can't throw
+}
+
+template <class T>
+Stack<T>::Stack(const Stack<T> &other)
+    : v_(NewCopy(other.v_, other.vsize_, other.vused_)), vsize_(other.vsize_),
+      vused_(other.vused_) {}
+
+template <class T> Stack<T> &Stack<T>::operator=(const Stack<T> &other) {
+  if (this != other) { // ensures safety and independence of the program's
+                       // state.
+    T *new_v = NewCopy(other.v_, other.vsize_, other.vused_);
+    delete v_;
+    v_ = new_v;
+    vsize_ = other.vsize_;
+    vsize_ = other.vused_;
+  }
+  return *this;
 }
--- leetcode/hashing/ProdArrayExceptSelf.cpp
+++ leetcode/hashing/ProdArrayExceptSelf.cpp
@@ -20,7 +20,6 @@ vector<int> productExceptSelf(vector<int>& nums) {
         prefix = prefix * nums[i];
     }
 
-    
     int postfix = 1;
 
     for (int i = n - 1; i >= 0; i--) {
@@ -33,21 +32,14 @@ vector<int> productExceptSelf(vector<int>& nums) {
 
 
 int main() {
+
     vector<int> nums1 = {1, 2, 3, 4};
-    vector<int> result1 = productExceptSelf(nums1);
-    cout << "Output for nums1: ";
-    for (int num : result1) {
-        cout << num << " ";
-    }
-    cout << endl;
+    assert((productExceptSelf(nums1) == std::vector<int>{24,12,8,6}));
 
     vector<int> nums2 = {-1, 1, 0, -3, 3};
-    vector<int> result2 = productExceptSelf(nums2);
-    cout << "Output for nums2: ";
-    for (int num : result2) {
-        cout << num << " ";
-    }
-    cout << endl;
+    assert((productExceptSelf(nums2) == std::vector<int>{0,0,9,0,0}));
+
+    std::cout << "All test cases passed successfully!" << std::endl;
 
     return 0;
 }
