ea477325c2f120246a0c11c5a424b268d8ec1020
Saad-Hussain31 <saadhussain31@gmail.com>
2024-03-24 11:51:26 +0000

    write decode function for string

--- leetcode/hashing/EncodeDecodeString.cpp
+++ leetcode/hashing/EncodeDecodeString.cpp
@@ -15,20 +15,34 @@ string encode(vector<string>& strs) {
 
 }
 
+
 vector<string> decode(string s) {
-    
+    vector<string> result;
+    int i = 0;
+
+    while(i < s.size()) {
+        int j = i;
+        while(s[j] != '#') {
+            j++;
+        }
+        int length = stoi(s.substr(i, j-i));
+        string str = s.substr(j+1, length);
+        result.push_back(str);
+        i = j+1+length;
+    }
+    return result;
 }
 
 int main() {
-    vector<string> input = {"abc", "def", "ghi"};
+    vector<string> input = {"neet", "co#de"};
     
     
     string encoded_str = encode(input);
-    assert(encoded_str == "3#abc3#def3#ghi");
+    assert(encoded_str == "4#neet5#co#de");
     cout << "Encoded string: " << encoded_str << endl;
     
     vector<string> decoded_strs = decode(encoded_str);
-    // assert(decoded_strs == input);
+    assert(decoded_strs == input);
     cout << "Decoded strings: ";
     for (const auto& str : decoded_strs) {
         cout << str << " ";
