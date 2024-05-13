1bf6b104c36d10602df6fc7e4f563b73f435c162
Saad-Hussain31 <saadnasir31@gmail.com>
2024-05-13 07:13:23 +0000

    Problem with raw ptrs

--- coreFeatures/sharedPtr.cpp
+++ coreFeatures/sharedPtr.cpp
@@ -1,28 +1,75 @@
 /*
- * 2 pointers can point to same resource.  
+ * 2 pointers can point to same resource.
  * when all ptrs get freed, the destructor will be auto called.
-*/
+ */
 #include <iostream>
+#include <memory>
 #include <string>
 #include <vector>
-#include <memory>
-
+using namespace std;
 class UDT {
-public: 
-    UDT() { std::cout << "UDT Created \n";}
-    ~UDT() { std::cout << "UDT destroyed\n" ;}
+public:
+  UDT() { std::cout << "UDT Created \n"; }
+  ~UDT() { std::cout << "UDT destroyed\n"; }
 };
+
+/*
 
 int main() {
 
-    std::shared_ptr<UDT> ptr = std::make_shared<UDT>(); //ptr is pointing to a resource
+    std::shared_ptr<UDT> ptr = std::make_shared<UDT>(); //ptr is pointing to a
+resource
     {
-        std::shared_ptr<UDT> ptr2 = ptr; // ptr2 is pointing to same above resource. hence the resource is shared by 2 pointers. wasnt allowed with unique ptr. 
-        std::cout << "reference count = " << ptr2.use_count() << "\n";//after this line, scope of ptr2 is destroyed
+        std::shared_ptr<UDT> ptr2 = ptr; // ptr2 is pointing to same above
+resource. hence the resource is shared by 2 pointers. wasnt allowed with unique
+ptr. std::cout << "reference count = " << ptr2.use_count() << "\n";//after this
+line, scope of ptr2 is destroyed
     }
-    // std::cout << "reference count = " << ptr2.use_count() << "\n";// error coz ptr2 no longer exist.
-    std::cout << "reference count = " << ptr.use_count() << "\n";
-    
-    
+    // std::cout << "reference count = " << ptr2.use_count() << "\n";// error
+coz ptr2 no longer exist. std::cout << "reference count = " << ptr.use_count()
+<< "\n";
+
+
     return 0;
 }
+*/
+
+// Bo Qian Shared Pointer Lesson
+
+/*
+the real problem with raw ptrs is with delete keyword. if we delete the ptr
+early then we have dangling ptr and if we forget to delete it then we have a
+memory leak.
+
+
+
+*/
+
+class Dog {
+  string mName;
+
+public:
+  Dog(string name) {
+    cout << "Dog is created " << name << endl;
+    mName = name;
+  }
+
+  Dog() {
+    cout << "Nameless dog is created " << endl;
+    mName = "nameless";
+  }
+
+  ~Dog() { cout << "Dog is destroyed " << mName << endl; }
+
+  void bark() { cout << "Dog " << mName << " rules!" << endl; }
+};
+
+void foo() {
+  Dog *p = new Dog("Gunner");
+  //...
+  delete p;
+  //..
+  p->bark(); // p is a dangling ptr -- undefined behavior
+}
+
+int main() {}
